96ac4999f5fbe62e959efff598c0cc96f943fb38
Thane Heninger <tsheninger@control4.com>
2020-10-30 11:00:44 -0600

    Initial Commit

---
+++ TeamDiceRoller/TeamDiceRollerApp.swift
@@ -0,0 +1,20 @@
+//
+//  TeamDiceRollerApp.swift
+//  TeamDiceRoller
+//
+//  Created by Thane Heninger on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct TeamDiceRollerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
