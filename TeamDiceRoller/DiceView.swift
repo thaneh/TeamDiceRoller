cfb1ee265c4b2aefe1df5e93657cf231cf2545d6
Jim Norman <jnorman@control4.com>
2020-10-30 12:53:32 -0600

    Changes

---
+++ TeamDiceRoller/DiceView.swift
@@ -0,0 +1,20 @@
+//
+//  DiceView.swift
+//  TeamDiceRoller
+//
+//  Created by Jim Norman on 10/30/20.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiceView()
+    }
+}
